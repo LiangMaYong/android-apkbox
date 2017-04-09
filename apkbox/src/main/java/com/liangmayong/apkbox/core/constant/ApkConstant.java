88eab101fd83a3df9bd3545460a9fcd948a03ba7
liangmayong <ibeam@qq.com>
2017-04-09 23:20:42 +0800

    add service count proxy0service -> proxy20service

--- apkbox/src/main/java/com/liangmayong/apkbox/core/constant/ApkConstant.java
+++ apkbox/src/main/java/com/liangmayong/apkbox/core/constant/ApkConstant.java
@@ -5,10 +5,10 @@ package com.liangmayong.apkbox.core.constant;
  */
 public interface ApkConstant {
 
-    String EXTRA_APK_PATH = "box_extra_apk_path";
-    String EXTRA_APK_PROXY = "box_extra_apk_proxy";
-    String EXTRA_APK_EXTRAS = "box_extra_apk_extras";
-    String EXTRA_APK_MODIFIED = "box_extra_apk_modified";
-    String EXTRA_APK_TARGET_INTENT = "box_extra_apk_target_intent";
+    String EXTRA_APK_PATH = "apkbox_extra_apk_path";
+    String EXTRA_APK_PROXY = "apkbox_extra_apk_proxy";
+    String EXTRA_APK_EXTRAS = "apkbox_extra_apk_extras";
+    String EXTRA_APK_MODIFIED = "apkbox_extra_apk_modified";
+    String EXTRA_APK_TARGET_INTENT = "apkbox_extra_apk_target_intent";
 
 }
