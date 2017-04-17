fd9aeccfdcfe104b6155a1c096773b6d7a910922
liangmayong <liangmayong@myhexin.com[core]    compression = -1>
2017-04-17 15:05:00 +0800

    fix bug by receiver

--- apkbox/src/main/java/com/liangmayong/apkbox/core/loader/ApkReceiver.java
+++ apkbox/src/main/java/com/liangmayong/apkbox/core/loader/ApkReceiver.java
@@ -1,25 +1,25 @@
 /**
- The MIT License (MIT)
-
- Copyright (c) 2017 LiangMaYong ( ibeam@qq.com )
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017 LiangMaYong ( ibeam@qq.com )
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  **/
 package com.liangmayong.apkbox.core.loader;
 
@@ -58,7 +58,7 @@ public final class ApkReceiver {
         if (BROADCAST_RECEIVER_MAP.containsKey(key)) {
             LinkedList<BroadcastReceiver> receivers = BROADCAST_RECEIVER_MAP.get(key);
             for (int i = 0; i < receivers.size(); i++) {
-                context.getApplicationContext().unregisterReceiver(receivers.get(i));
+                context.unregisterReceiver(receivers.get(i));
             }
             BROADCAST_RECEIVER_MAP.remove(key);
         }
@@ -80,6 +80,8 @@ public final class ApkReceiver {
             return;
         }
         if (loaded != null) {
+            Context ctx = context.getApplicationContext();
+            CONTEXT_MAP.put(key, ctx);
             Map<String, IntentFilter> filters = loaded.getFilters();
             LinkedList<BroadcastReceiver> receivers = new LinkedList<BroadcastReceiver>();
             for (Map.Entry<String, IntentFilter> entry : filters.entrySet()) {
@@ -99,7 +101,7 @@ public final class ApkReceiver {
                                 intentFilter.addAction(action);
                             }
                             BroadcastReceiver broadcastReceiver = (BroadcastReceiver) clazz.newInstance();
-                            context.getApplicationContext().registerReceiver(broadcastReceiver, intentFilter);
+                            ctx.registerReceiver(broadcastReceiver, intentFilter);
                             receivers.add(broadcastReceiver);
                         } catch (Exception e) {
                         }
