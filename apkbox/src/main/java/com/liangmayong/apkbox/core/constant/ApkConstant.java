5aed887a297a411db334096f36e6843aae2609a1
liangmayong <liangmayong@myhexin.com[core]    compression = -1>
2017-04-17 15:36:38 +0800

    theme

--- apkbox/src/main/java/com/liangmayong/apkbox/core/constant/ApkConstant.java
+++ apkbox/src/main/java/com/liangmayong/apkbox/core/constant/ApkConstant.java
@@ -31,6 +31,7 @@ public interface ApkConstant {
     String EXTRA_APK_PATH = "apkbox_extra_path";
     String EXTRA_APK_PROXY = "apkbox_extra_proxy";
     String EXTRA_APK_EXTRAS = "apkbox_extra_extras";
+    String EXTRA_APK_THEME = "apkbox_extra_theme";
     String EXTRA_APK_MODIFIED = "apkbox_extra_modified";
     String EXTRA_APK_LAUNCH_MODE = "apkbox_extra_launch_mode";
     String EXTRA_APK_TARGET_INTENT = "apkbox_extra_target_intent";
--- apkbox/src/main/java/com/liangmayong/apkbox/hook/modify/ApkActivityInfoModifier.java
+++ apkbox/src/main/java/com/liangmayong/apkbox/hook/modify/ApkActivityInfoModifier.java
@@ -1,25 +1,25 @@
 /**
- The MIT License (MIT)
-
- Copyright (c) 2017 LiangMaYong ( ibeam@qq.com )
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017 LiangMaYong ( ibeam@qq.com )
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  **/
 package com.liangmayong.apkbox.hook.modify;
 
@@ -38,6 +38,7 @@ import android.view.LayoutInflater;
 import android.view.Window;
 
 import com.liangmayong.apkbox.core.ApkLoaded;
+import com.liangmayong.apkbox.core.constant.ApkConstant;
 import com.liangmayong.apkbox.core.resources.ApkLayoutInflater;
 import com.liangmayong.apkbox.reflect.ApkReflect;
 import com.liangmayong.apkbox.utils.ApkLogger;
@@ -123,6 +124,7 @@ public class ApkActivityInfoModifier {
 
         try {
             int mThemeResource = activityInfo.getThemeResource();
+            target.getIntent().putExtra(ApkConstant.EXTRA_APK_THEME, mThemeResource);
             ApkReflect.setField(ContextThemeWrapper.class, target, "mThemeResource", mThemeResource);
             ContextThemeWrapper contextThemeWrapper = new ContextThemeWrapper(target, mThemeResource);
             ApkReflect.setField(target.getClass(), target, "mTheme", contextThemeWrapper.getTheme());
